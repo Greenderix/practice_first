28822c981b2b93604e4b007cf35b23ae9da6d602
Evgeny <grinderix@yandex.ru>
2022-05-28 17:48:33 +0300

    Solved

---
+++ python/pythonProject1/main.py
@@ -0,0 +1,32 @@
+class HTML:
+    s = ""
+    def get_code(self):
+        return (self.s)
+    class body:
+        def __enter__(self):
+            HTML.s += "<body>\n"
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            HTML.s += "</body>\n"
+
+    class div:
+        def __enter__(self):
+            HTML.s += "<div>\n"
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            HTML.s += "</div>\n"
+
+    class p(object):
+        def __init__(self, ps):
+            HTML.s += "<p>" + ps + "</p>\n"
+
+
+html = HTML()
+with html.body():
+    with html.div():
+        with html.div():
+            html.p('Первая строка.')
+            html.p('Вторая строка.')
+        with html.div():
+            html.p('Третья строка.')
+print(html.get_code())
